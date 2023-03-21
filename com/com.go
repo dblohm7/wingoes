e40e852fafdc6a722e0ac3265b4e5445cae385bb
Aaron Klotz <aklotz@ohmsoft.com>
2023-03-21 14:33:22 -0600

    build tags
    Signed-off-by: Aaron Klotz <aklotz@ohmsoft.com>

--- com/com.go
+++ com/com.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build windows
+
 // Package com provides an idiomatic foundation for instantiating and invoking
 // COM objects.
 package com
--- com/mksyscall.go
+++ com/mksyscall.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build windows
+
 package com
 
 //go:generate go run golang.org/x/sys/windows/mkwinsyscall -output zsyscall_windows.go mksyscall.go
