7946d0ecbb2c1a673c9237c1472e04e49e542eaf
Aaron Klotz <aklotz@ohmsoft.com>
2023-03-21 14:22:08 -0600

    build_deps.go: add build_deps to assist with module imports
    Signed-off-by: Aaron Klotz <aklotz@ohmsoft.com>

---
+++ build_deps.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Tailscale Inc & AUTHORS. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package wingoes
+
+import (
+	_ "golang.org/x/tools/cmd/goimports"
+)
